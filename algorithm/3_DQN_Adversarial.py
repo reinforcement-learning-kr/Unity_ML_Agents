673a85bb15855ab0ab8715f15b1af641dbe2e4f2
kyushik <kyushikmin@gmail.com>
2019-07-15 10:55:16 +0900

    Upate DQN adversarial code and change mlagent from mlagents-env

--- algorithm/3_DQN_Adversarial.py
+++ algorithm/3_DQN_Adversarial.py
@@ -3,7 +3,6 @@ import random
 import datetime
 import time
 import tensorflow as tf
-import tensorflow.layers as layer
 from collections import deque
 from mlagents.envs import UnityEnvironment
 
@@ -27,6 +26,7 @@ target_update_step = 10000
 print_interval = 20
 save_interval = 5000
 
+epsilon_init = 1.0
 epsilon_min = 0.1
 
 date_time = str(datetime.date.today()) + '_' + \
@@ -42,19 +42,16 @@ load_path = "../saved_models/" + game + "/2019-02-20_16_27_5_DQN/model/model.ckp
 
 class Model():
     def __init__(self, model_name):
-        self.state_size = state_size
-        self.action_size = action_size
-
-        self.input = tf.placeholder(shape=[None, self.state_size], dtype=tf.float32)
+        self.input = tf.placeholder(shape=[None, state_size], dtype=tf.float32)
 
         with tf.variable_scope(name_or_scope=model_name):
-            self.fc1 = layer.dense(self.input,512,activation=tf.nn.relu)
-            self.fc2 = layer.dense(self.fc1,512,activation=tf.nn.relu)
-            self.fc3 = layer.dense(self.fc2,512,activation=tf.nn.relu)
-            self.Q_Out = layer.dense(self.fc3,self.action_size,activation=None)
+            self.fc1 = tf.layers.dense(self.input,512,activation=tf.nn.relu)
+            self.fc2 = tf.layers.dense(self.fc1,512,activation=tf.nn.relu)
+            self.fc3 = tf.layers.dense(self.fc2,512,activation=tf.nn.relu)
+            self.Q_Out = tf.layers.dense(self.fc3,action_size,activation=None)
         self.predict = tf.argmax(self.Q_Out, 1)
 
-        self.target_Q = tf.placeholder(shape=[None, self.action_size], dtype=tf.float32)
+        self.target_Q = tf.placeholder(shape=[None, action_size], dtype=tf.float32)
 
         self.loss = tf.losses.mean_squared_error(self.target_Q, self.Q_Out)
         self.UpdateModel = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
@@ -62,122 +59,82 @@ class Model():
 
 class DQNAgent():
     def __init__(self):
-        self.state_size = state_size
-        self.action_size = action_size
-
         self.model1 = Model("Q1")
         self.target_model1 = Model("target1")
-        self.model2 = Model("Q2"e)
+        self.model2 = Model("Q2")
         self.target_model2 = Model("target2")
 
         self.memory1 = deque(maxlen=mem_maxlen)
         self.memory2 = deque(maxlen=mem_maxlen)
 
-        self.batch_size = batch_size
-        self.run_episode = run_episode
-
         self.sess = tf.Session()
         self.init = tf.global_variables_initializer()
         self.sess.run(self.init)
 
-        self.epsilon = epsilon
-        self.epsilon_min = epsilon_min
-        self.discount_factor = discount_factor
+        self.epsilon = epsilon_init
 
-        self.load_model = load_model
         self.Saver = tf.train.Saver
-        self.save_path = save_path
-        self.load_path = load_path
-        self.Summary, self.Merge = self.make_Summary()
+        self.Summary, self.Merge = self.Make_Summary()
 
         self.update_target()
 
-        if self.load_model == True:
-            self.Saver.restore(self.sess, self.load_path)
+        if load_model == True:
+            self.Saver.restore(self.sess, load_path)
 
     def get_action(self, state1, state2, train_mode=True):
         if train_mode == True and self.epsilon > np.random.rand():
-            random_action1 = np.random.randint(0, self.action_size)
-            random_action2 = np.random.randint(0, self.action_size)
+            random_action1 = np.random.randint(0, action_size)
+            random_action2 = np.random.randint(0, action_size)
             return random_action1, random_action2
         else:
             predict1 = self.sess.run(self.model1.predict, 
                                      feed_dict={self.model1.input: [state1]})
             predict2 = self.sess.run(self.model2.predict, 
                                      feed_dict={self.model2.input: [state2]})
-            return np.asscalar(predict1), predict2
+            return np.asscalar(predict1), np.asscalar(predict2)
 
     def append_sample(self, data1, data2):
         self.memory1.append((data1[0], data1[1], data1[2], data1[3], data1[4]))
         self.memory2.append((data2[0], data2[1], data2[2], data2[3], data2[4]))
 
     def save_model(self):
-        self.Saver.save(self.sess, self.save_path + "/model/model.ckpt")
+        self.Saver.save(self.sess, save_path + "/model/model.ckpt")
 
-    def train_model(self, done):
+    def train_model(self, model, target_model, memory, done):
         if done:
-            if self.epsilon > self.epsilon_min:
-                self.epsilon -= 1 / (self.run_episode - start_train_episode)
+            if self.epsilon > epsilon_min:
+                self.epsilon -= 1 / (run_episode - start_train_episode)
 
-        mini_batch1 = random.sample(self.memory1, self.batch_size)
+        mini_batch = random.sample(memory, batch_size)
 
-        states1 = []
-        actions1 = []
-        rewards1 = []
-        next_states1 = []
-        dones1 = []
+        states = []
+        actions = []
+        rewards = []
+        next_states = []
+        dones = []
 
-        for i in range(self.batch_size):
-            states1.append(mini_batch1[i][0])
-            actions1.append(mini_batch1[i][1])
-            rewards1.append(mini_batch1[i][2])
-            next_states1.append(mini_batch1[i][3])
-            dones1.append(mini_batch1[i][4])
+        for i in range(batch_size):
+            states.append(mini_batch[i][0])
+            actions.append(mini_batch[i][1])
+            rewards.append(mini_batch[i][2])
+            next_states.append(mini_batch[i][3])
+            dones.append(mini_batch[i][4])
 
-        target1 = self.sess.run(self.model1.Q_Out, feed_dict={self.model1.input: states1})
-        target_val1 = self.sess.run(self.target_model1.Q_Out, 
-                                    feed_dict={self.target_model1.input: next_states1})
+        target = self.sess.run(model1.Q_Out, feed_dict={model.input: states})
+        target_val = self.sess.run(target_model.Q_Out, 
+                                    feed_dict={target_model.input: next_states})
 
-        for i in range(self.batch_size):
-            if dones1[i]:
-                target1[i][actions1[i]] = rewards1[i]
+        for i in range(batch_size):
+            if dones[i]:
+                target[i][actions[i]] = rewards[i]
             else:
-                target1[i][actions1[i]] = rewards1[i] + self.discount_factor * np.amax(target_val1[i])
+                target[i][actions[i]] = rewards[i] + discount_factor * np.amax(target_val[i])
 
-        _, loss1 = self.sess.run([self.model1.UpdateModel, self.model1.loss],
-                                feed_dict={self.model1.input: states1, 
-                                           self.model1.target_Q: target1})
+        _, loss = self.sess.run([model.UpdateModel, model.loss],
+                                feed_dict={model.input: states, 
+                                           model.target_Q: target})
 
-        mini_batch2 = random.sample(self.memory2, self.batch_size)
-
-        states2 = []
-        actions2 = []
-        rewards2 = []
-        next_states2 = []
-        dones2 = []
-
-        for i in range(self.batch_size):
-            states2.append(mini_batch2[i][0])
-            actions2.append(mini_batch2[i][1])
-            rewards2.append(mini_batch2[i][2])
-            next_states2.append(mini_batch2[i][3])
-            dones2.append(mini_batch2[i][4])
-
-        target2 = self.sess.run(self.model2.Q_Out, feed_dict={self.model2.input: states2})
-        target_val2 = self.sess.run(self.target_model2.Q_Out, 
-                                    feed_dict={self.target_model2.input: next_states2})
-
-        for i in range(self.batch_size):
-            if dones2[i]:
-                target2[i][actions2[i]] = rewards2[i]
-            else:
-                target2[i][actions2[i]] = rewards2[i] + self.discount_factor * np.amax(target_val2[i])
-
-        _, loss2 = self.sess.run([self.model2.UpdateModel, self.model2.loss],
-                                feed_dict={self.model2.input: states2, 
-                                           self.model2.target_Q: target2})
-
-        return loss1, loss2
+        return loss
 
     def update_target(self):
         for i in range(len(self.model1.trainable_var)):
@@ -186,7 +143,7 @@ class DQNAgent():
         for i in range(len(self.model2.trainable_var)):
             self.sess.run(self.target_model2.trainable_var[i].assign(self.model2.trainable_var[i]))
 
-    def make_Summary(self):
+    def Make_Summary(self):
         self.summary_loss1 = tf.placeholder(dtype=tf.float32)
         self.summary_reward1 = tf.placeholder(dtype=tf.float32)
         self.summary_loss2 = tf.placeholder(dtype=tf.float32)
@@ -277,7 +234,8 @@ if __name__ == '__main__':
             state2 = next_state2
 
             if episode > start_train_episode and train_mode:
-                loss1, loss2 = agent.train_model(done)
+                loss1 = agent.train_model(agent.model1, agent.target_model1, agent.memory1, done)
+                loss2 = agent.train_model(agent.model2, agent.target_model2, agent.memory2, done)
                 losses1.append(loss1)
                 losses2.append(loss2)
 
@@ -288,9 +246,9 @@ if __name__ == '__main__':
         rewards2.append(episode_rewards2)
 
         if episode % print_interval == 0 and episode != 0:
-            print("step: {} / episode: {} / reward1: {:.2f} / loss1: {:.4f} / reward2: {:.2f} \\
+            print("step: {} / episode: {} / reward1: {:.2f} / loss1: {:.4f} / reward2: {:.2f} \
                   / loss2: {:.4f} / epsilon: {:.3f}".format(step, episode, np.mean(rewards1), 
-                  np.mean(losses1), np.mean(rewards2), np.mean(losses2), agent.epsilon)))
+                  np.mean(losses1), np.mean(rewards2), np.mean(losses2), agent.epsilon))
             
             agent.Write_Summray(np.mean(rewards1), np.mean(losses1), 
                                 np.mean(rewards2), np.mean(losses2), episode)
